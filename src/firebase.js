f694a66bc225124bfefb728dc8749e28e5e8dae6
Mark Quiba <markjquiba@gmail.com>
2024-09-11 13:41:01 +0800

    chore: firebase setup with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBqm288IVFjRUwoEtBVet0ONYMsAAxX7XM',
-  authDomain: 'cast-app-chat.firebaseapp.com',
-  projectId: 'cast-app-chat',
-  storageBucket: 'cast-app-chat.appspot.com',
-  messagingSenderId: '780762228023',
-  appId: '1:780762228023:web:79406d16ba03739c2c26b2',
-  measurementId: 'G-W18LNW2RFR',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
